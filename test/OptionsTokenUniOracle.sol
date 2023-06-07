506c1da39432d1422af7d17f810a7de807b11a2c
vedev <80967041+magmacto@users.noreply.github.com>
2023-06-06 23:07:48 -0300

    fixed oracle price test

--- test/OptionsTokenUniOracle.sol
+++ test/OptionsTokenUniOracle.sol
@@ -3,89 +3,72 @@ pragma solidity 0.8.13;
 import './BaseTest.sol';
 import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
 
+import {UniswapV2Oracle} from "../contracts/options-token/oracles/UniswapV2Oracle.sol";
 import {Magma} from "../contracts/Magma.sol";
 import {OptionsToken} from "../contracts/options-token/OptionsToken.sol";
 import {TestERC20Mintable} from "./mocks/TestERC20Mintable.sol";
 import {BalancerOracle} from "../contracts/options-token/oracles/BalancerOracle.sol";
 import {IERC20Mintable} from "../contracts/options-token/interfaces/IERC20Mintable.sol";
 import {MockBalancerTwapOracle} from "./mocks/MockBalancerTwapOracle.sol";
-
+import {FaucetERC20d6} from "../contracts/mock/FaucetERC20d6.sol";
 contract OptionsTokenUniOracle is BaseTest {
 
-    Pair _pair;
+    FaucetERC20d6 usdc;
     Magma token;
     OptionsToken option;
+    UniswapV2Oracle oracle;
 
-    uint TAX_TOKEN_100K = 100_000 * 1e9;
-    uint TAX_TOKEN_1 = 1e9;
-    uint TAX_TOKEN_100 = 100e9;
-    uint TOKEN_100 = 100 * 1e18;
-    uint liquidityAdded;
+    address pairAddress;
+
     function setUp() public {
         factory = new PairFactory();
         WETH = new TestWETH();
+        usdc = new FaucetERC20d6("USDC", "USDC", 0);
         router2 = new Router2(address(factory), address(WETH));
         token = new Magma();
-        _pair = Pair(factory.getPair(address(token), address(WETH), false));
-    }
-
-    function testAddRemoveLiquidity() public {
-        token.approve(address(router2), TAX_TOKEN_100K);
-        WETH.approve(address(router2), TOKEN_100);
-
-        uint amountEthBefore = address(this).balance;
-        (,, liquidityAdded) =
-        router2.addLiquidityETH{value : TOKEN_100}(address(token), false, TAX_TOKEN_100K, 0, 0, address(this), block.timestamp);
-        uint amountEthAfter = address(this).balance;
-        uint amountEthAdded = amountEthBefore - amountEthAfter;
-        console2.log('amountEthBefore', amountEthBefore);
-        console2.log('amountEthAfter', amountEthAfter);
-        console2.log('amountEthAdded', amountEthAdded);
-        assertEq(amountEthAfter, amountEthBefore - TOKEN_100);
-        assertEq(amountEthAdded, TOKEN_100);
-
-        _pair.approve(address(router2), liquidityAdded);
-        router2.removeLiquidityETHSupportingFeeOnTransferTokens(address(token), false, liquidityAdded, 0, 0, address(this), block.timestamp);
-        uint amountEthAfterRemove = address(this).balance;
-        console2.log('amountEthAfterRemove', amountEthAfterRemove);
-        assertEq(amountEthAfterRemove, amountEthBefore-1000000);
+        pairAddress = factory.createPair(address(token), address(usdc), false);
+        pair = Pair(pairAddress);
+        oracle = new UniswapV2Oracle(pairAddress);
 
     }
 
     function testSwap() public{
 
-        token.approve(address(router2), TAX_TOKEN_100K);
-        WETH.approve(address(router2), TOKEN_100);
+        token.mint(address(this), 100_000e18);
+        usdc.mint(100_000e6);
 
-        uint amountEthBefore = address(this).balance;
-        (,, liquidityAdded) =
-        router2.addLiquidityETH{value : TOKEN_100}(address(token), false, TAX_TOKEN_100K, 0, 0, address(this), block.timestamp);
-        uint amountEthAfter = address(this).balance;
-        uint amountEthAdded = amountEthBefore - amountEthAfter;
-        console2.log('amountEthBefore', amountEthBefore);
-        console2.log('amountEthAfter', amountEthAfter);
-        console2.log('amountEthAdded', amountEthAdded);
-        assertEq(amountEthAfter, amountEthBefore - TOKEN_100);
-        assertEq(amountEthAdded, TOKEN_100);
+        token.approve(address(router2), 100_000e18);
+        usdc.approve(address(router2), 100_000e6);
+
+        router2.addLiquidity(address(token), address(usdc), false, 100_000e18, 100_000e6, 0, 0, address(this), block.timestamp);
 
         Router.route[] memory routes = new Router.route[](1);
-        routes[0] = Router.route(address(token), address(WETH), false);
-        uint256[] memory expectedOutput = router2.getAmountsOut(TAX_TOKEN_100, routes);
-        token.approve(address(router2), TAX_TOKEN_100);
+        routes[0] = Router.route(address(usdc), address(token), false);
 
-        console2.log('tokenBalanceBeforeSwap', token.balanceOf(address(this))/1e9);
-        router2.swapExactTokensForETHSupportingFeeOnTransferTokens(TAX_TOKEN_100, expectedOutput[1], routes, address(this), block.timestamp);
-        console2.log('expectedOutput token', expectedOutput[0]/1e9);
-        console2.log('expectedOutput eth', expectedOutput[1]);
-        console2.log('tokenBalanceAfterSwap', token.balanceOf(address(this))/1e9);
-        console2.log('amountEthAfterSwap', (address(this).balance - amountEthAfter));
+        uint buyAmount = 100e6;
+        usdc.mint(buyAmount);
+        usdc.approve(address(router2), buyAmount);
+        uint swapAmount = 1e6;
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
+        router2.swapExactTokensForTokens(swapAmount, 0, routes, address(this), block.timestamp);
+        vm.warp(block.timestamp+3600);
 
-        _pair.approve(address(router2), liquidityAdded);
-        router2.removeLiquidityETHSupportingFeeOnTransferTokens(address(token), false, liquidityAdded, 0, 0, address(this), block.timestamp);
-        uint amountEthAfterRemove = address(this).balance;
-        console2.log('amountTokenAfterRemove', token.balanceOf(address(this)));
-        console2.log('amountEthAfterRemove', amountEthAfterRemove/1e18);
-        assertEq(amountEthAfterRemove, amountEthBefore - 999_004);
+        uint256[] memory prices = oracle.price(address(usdc), 100e6, block.timestamp-12000, 30);
+        uint priceInWei = prices[0];
+        console.log("priceInWei", priceInWei);
 
     }
 }
